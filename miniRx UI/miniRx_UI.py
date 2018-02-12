9978d8b60e779c2d9c368ace7de4fae53ed07b4e
LogicalShark <malder@andrew.cmu.edu>
2018-02-12 00:02:17 -0500

    nothing

--- miniRx UI/miniRx_UI.py
+++ miniRx UI/miniRx_UI.py
@@ -1,7 +1,7 @@
 # Updated Animation Starter Code
 
 from tkinter import *
-import OpenCV-Face-Recognition-Python.py
+import facerecognition
 ####################################
 # Color palette
 darkerRed  = "#C14242"
