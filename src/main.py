6ede3534d4d6b3c8261bab9266f53a0cfc4488c8
Candia Gu <candi37@gmail.com>
2018-02-12 01:58:25 -0500

    arduino code

--- src/main.py
+++ src/main.py
@@ -1,16 +1,30 @@
-# import serial.tools.list_ports
-# ports = list(serial.tools.list_ports.comports())
-# for p in ports:
-#     print(p)
+# # import serial.tools.list_ports
+# # ports = list(serial.tools.list_ports.comports())
+# # for p in ports:
+# #     print(p)
 import serial
 import time
-ser = serial.Serial('COM11', 9600)
- 
+ser = serial.Serial('/dev/ttyS10', 9600)
+
 while 1:
-    try:
-        print "something"
-        print ser.readline()
-        time.sleep(1)
-    except ser.SerialTimeoutException:
-        print 'Data could not be read'
-        time.sleep(1)
+	try:
+	    #print "something"
+	    #print ser.readline()
+	    ser.write('5')
+	    time.sleep(1)
+	    ser.write('6')
+	    time.sleep(1)
+	except ser.SerialTimeoutException:
+	    print 'Data could not be read'
+	    time.sleep(1)
+
+
+# import serial, time
+# arduino = serial.Serial('/dev/ttyS10', 9600, timeout=.1)
+# time.sleep(1) #give the connection a second to settle
+# arduino.write("Hello from Python!")
+# while True:
+# 	data = arduino.readline()
+# 	if data:
+# 		print data.rstrip('\n') #strip out the new lines for now
+# 		# (better to do .read() in the long run for this reason
---
+++ src/motorControl/motorControl.ino
@@ -0,0 +1,55 @@
+// defines pins numbers
+const int stepPin1 = 3; 
+const int dirPin1 = 4;
+const int stepPin2 = 5; 
+const int dirPin2 = 7;
+const int stepPin3 = 11; 
+const int dirPin3 = 12;
+
+void setup(){
+  // Open serial connection.
+  Serial.begin(9600);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+  pinMode(stepPin3, OUTPUT);
+  pinMode(dirPin3, OUTPUT);
+ 
+}
+
+void loop(){
+  if(Serial.available() > 0) {
+    char var = Serial.read();
+    turnClockwiseOnce(var);
+  }
+}
+
+void turnClockwiseOnce(int motor){
+  int stepPin; int dirPin;
+  switch(motor){
+    case '1':
+      stepPin = stepPin1;
+      dirPin = dirPin1;
+      break;
+    case '2':
+      stepPin = stepPin2;
+      dirPin = dirPin2;
+      break;
+    case '3':
+      stepPin = stepPin3;
+      dirPin = dirPin3;
+      break;
+  }
+
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+}
+
